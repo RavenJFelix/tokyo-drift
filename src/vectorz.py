9f765caed1b8b23de5012366b6f30e3b0a2e2029
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-09-05 10:21:04 -0500

    Add vector utility algorithms

--- src/vectorz.py
+++ src/vectorz.py
@@ -1,3 +1,6 @@
+import math as math
+
+
 class Vec2d:
     # Holds a 2 dimensional vector
     x = 0
@@ -19,4 +22,29 @@ class Vec2d:
         scaled_x = self.x * scale_factor
         scaled_y = self.y * scale_factor
         return Vec2d(scaled_x, scaled_y)
+
+    def get_counterclockwise_orthog(self):
+        return Vec2d(-self.y, self.x)
+
+    def get_magnitude(self):
+        # homebrew probably seek another library or put into a custom
+        # math modue
+        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2))
+
+    def get_unit(self):
+        return self.scale(1.0/self.get_magnitude())
+
+    def get_counterclockwise_orthog_unit(self):
+        return self.get_counterclockwise_orthog().get_unit()
+
+
+
+
+    def inverse(self):
+        return Vec2d(-self.x, -self.y)
+
+
+
+
+
 
